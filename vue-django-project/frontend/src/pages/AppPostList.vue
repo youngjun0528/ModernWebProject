2cadb304ace3d531e63da5fca2002d2a6386504d
youngjun0528 <youngjun0528@gmail.com>
2021-12-06 08:27:21 +0000

    vue-django npm build to backend folder

---
+++ vue-django-project/frontend/src/pages/AppPostList.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left color="indigo" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <PostList />
+    </v-main>
+
+    <v-footer app>
+      <span>&copy; 2021</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import PostList from "../components/PostList.vue";
+
+export default {
+  components: { PostList },
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
--- vue-django-project/frontend/vue.config.js
+++ vue-django-project/frontend/vue.config.js
@@ -1,10 +1,64 @@
+const FileManagerPlugin = require('filemanager-webpack-plugin-fixed');
+
 module.exports = {
-    // options...
-    devServer: {
-        disableHostCheck: true
+  // options...
+  devServer: {
+    disableHostCheck: true,
+    index: 'home.html',
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+  outputDir: 'dist',
+  publicPath: '/',
+  assetsDir: 'static', // default : ''
+
+  pages: {
+    home: {
+      template: 'public/index.html',
+      entry: 'src/pages/main_home.js',
+      filename: 'home.html',
+      title: 'VueDjangoProject/home.html',
+      minify: false,
     },
 
-    transpileDependencies: [
-      'vuetify'
+    post_list: {
+      template: 'public/index.html',
+      entry: 'src/pages/main_post_list.js',
+      filename: 'post_list.html',
+      title: 'VueDjangoProject/post_list.html',
+      minify: false,
+    },
+
+    post_detail: {
+      template: 'public/index.html',
+      entry: 'src/pages/main_post_detail.js',
+      filename: 'post_detail.html',
+      title: 'VueDjangoProject/post_detail.html',
+      minify: false,
+    }
+  },
+
+  configureWebpack: {
+    plugins: [
+      new FileManagerPlugin({
+        onStart: {
+          delete: [
+            '../backend/static/**/',
+            '../backend/templates/**/'
+          ]
+        },
+        onEnd: {
+          copy: [
+            { source: 'dist/static', destination: '../backend/static/' },
+            { source: 'dist/favicon.ico', destination: '../backend/static/img/' },
+            { source: 'dist/home.html', destination: '../backend/templates/' },
+            { source: 'dist/post*.html', destination: '../backend/templates/blog/' },
+          ]
+        }
+      })
     ]
+  }
 }
